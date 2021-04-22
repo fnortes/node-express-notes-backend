97eecaf82c6d826a9a98b5f9221f7a5965e7bfbe
Francisco Nortes Belmonte <francisco.nortes@gmail.com>
2021-04-22 15:35:10 +0200

    Add config to deploy in Heroku

--- index.js
+++ index.js
@@ -84,7 +84,8 @@ app.post('/api/notes', (req, res) => {
 
 app.use(notFound)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
